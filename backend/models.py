6b4cb62389c8b2530761f23e94dc695d5a8db4d6
AJB <48493571+AngryJellyBean@users.noreply.github.com>
2025-03-23 16:23:28 -0500

    backend restructured frontend deleted

--- backend/models.py
+++ backend/models.py
@@ -1,16 +1,21 @@
 from config import db
 
 class Contact(db.Model):
+
     id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(80), unique = False, nullable = False)
     last_name = db.Column(db.String(80), unique = False, nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
-
+    notes = db.Column(db.String(250), unique = False, nullable = True)
+    img_url = db.Column(db.String(250), unique = False, nullable = True)
+        
     def to_json(self):
         
         return {
             "id": self.id,
             "firstName": self.first_name,
             "lastName": self.last_name,
-            "email": self.email
+            "email": self.email,
+            "notes": self.notes,
+            "imgUrl": self.img_url
         }
--- backend/routes.py
+++ backend/routes.py
@@ -2,26 +2,41 @@ from flask import request, jsonify
 from config import app, db
 from models import Contact
 
-@app.route("/contacts", methods = ["GET"])
+# GET ALL CONTACTS
+@app.route("/api/contacts", methods = ["GET"])
 def get_contacts():
     contacts = Contact.query.all()
     json_contacts = list(map(lambda x: x.to_json(), contacts))
     return jsonify({"contacts": json_contacts})
 
-@app.route("/create_contact", methods = ["POST"])
+# CREATE CONTACT
+@app.route("/api/contacts", methods = ["POST"])
 def create_contact():
-
+    
     if request.json == None:
         return jsonify({"message":"Missing JSON"}), 400
     
-    first_name = request.json.get("firstName");
+    first_name = request.json.get("firstName")
     last_name = request.json.get("lastName")
     email = request.json.get("email")
+    notes = request.json.get("notes")
+    img_url = request.json.get("imgUrl")
 
-    if not first_name or not last_name or not email:
-        return jsonify({"message": "You must include a first name, last name, and email "}), 400
+    if not first_name or not last_name:
+        return jsonify({"message": "You must include a first name and a last name"}), 400
+    
+    if not email:
+        email = None
 
-    new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
+    if not notes:
+        notes = None
+
+    # Fetch contact image
+    if not img_url:
+        img_url = f"https://ui-avatars.com/api/?name={first_name}+{last_name}"
+
+    new_contact = Contact(first_name=first_name, last_name=last_name, email=email, notes=notes, img_url=img_url)
+
     try:
         db.session.add(new_contact)
         db.session.commit()
@@ -30,7 +45,8 @@ def create_contact():
 
     return jsonify({"message": "User created!"}), 201
 
-@app.route("/update_contact/<int:user_id>", methods = ["PATCH"])
+# UPDATE CONTACT
+@app.route("/api/contacts/<int:user_id>", methods = ["PATCH"])
 def update_contact(user_id):
     contact = Contact.query.get(user_id)
     if not contact:
@@ -40,12 +56,15 @@ def update_contact(user_id):
     contact.first_name = data.get("firstName", contact.first_name)
     contact.last_name = data.get("lastName", contact.last_name)
     contact.email = data.get("email", contact.email)
+    contact.notes = data.get("notes", contact.notes)
+    contact.img_url = data.get("imgUrl", contact.img_url)
 
     db.session.commit()
     
     return jsonify({"message":"User updated!"}), 201
 
-@app.route("/delete_contact/<int:user_id>", methods = ["DELETE"])
+# DELETE CONTACT
+@app.route("/api/contacts/<int:user_id>", methods = ["DELETE"])
 def delete_contact(user_id):
     contact = Contact.query.get(user_id)
     if not contact:
